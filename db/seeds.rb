d953ab2efdd7bb5ac8dfd680355b385bfa90e3d4
Tafara Mafemba <tafara.maf@gmail.com>
2024-11-22 17:03:28 -0800

    Fix db credentials and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,13 +8,6 @@ end
 City.update(1, collection_point_address: "Hand2hand,  Roadport")
 City.update(2, collection_point_address: "Shop no 1 Crownmall, 87c Herbert Chitepo Street, Manica Post Building")
 
-Admin.create!(email: 'teller1@hand2handcourierszw.com', password: 'password', password_confirmation: 'password')
-Admin.create!(email: 'teller2@hand2handcourierszw.com', password: 'password', password_confirmation: 'password')
-Admin.create!(email: 'teller3@hand2handcourierszw.com', password: 'password', password_confirmation: 'password')
-Admin.create!(email: 'teller4@hand2handcourierszw.com', password: 'password', password_confirmation: 'password')
-Admin.create!(email: 'teller5@hand2handcourierszw.com', password: 'password', password_confirmation: 'password')
-Admin.create!(email: 'teller6@hand2handcourierszw.com', password: 'password', password_confirmation: 'password')
-
 Category.create!(name: 'Letter/Documents', base_price: 3, minimum_cost: 5, additional_cost_per_gram: 0, fee_percentage: 0)
 Category.create!(name: 'Parcel', base_price: 3, minimum_cost: 10, additional_cost_per_gram: 0, fee_percentage: 0)
 Category.create!(name: 'Cash', base_price: 0, minimum_cost: 5, additional_cost_per_gram: 0, fee_percentage: 5)
